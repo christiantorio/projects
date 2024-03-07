5c70b630ecfeaba06a293515e6e94f10b30593a2
Christian Torio <christiantorio@aol.com>
2024-03-06 19:45:12 -0800

    updating vm terraform

--- terraform/gcp/vm/main.tf
+++ terraform/gcp/vm/main.tf
@@ -12,7 +12,58 @@ provider "google" {
   project = "ctorio-devops-test"
   region  = "us-central1"
 }
+resource "google_compute_instance" "instance-20240307-034020" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-20240307-034020"
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240213"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+  name         = "instance-20240307-034020"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/ctorio-devops-test/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "devops-test-1@ctorio-devops-test.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-central1-a"
 }
